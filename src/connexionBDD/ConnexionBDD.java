3febe6589b2f4ee8f23574a423e4398e00a38690
Riwane <riwane.b96@gmail.com>
2016-02-08 11:11:40 +0100

    test riwane

--- src/connexionBDD/ConnexionBDD.java
+++ src/connexionBDD/ConnexionBDD.java
@@ -3,9 +3,6 @@ package connexionBDD;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
-
 public class ConnexionBDD {
 	private String userMailAdress;
 	private String password;
